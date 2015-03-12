fdaed0207eb8f9889cad4bc5253a8729203a5773
Benny Cardenas <benitocardenas@gmail.com>
2015-03-12 09:31:09 -0500

    Functions beginning

---
+++ arithmetic.php
@@ -0,0 +1,38 @@
+<?php 
+
+$a = 4;
+$b = 7;
+
+
+function add($a, $b) {
+    echo $a + $b;
+}
+
+function subtract($a, $b) {
+    // Add code here
+    echo $a - $b;
+}
+
+function multiply($a, $b) {
+    // Add code here
+    echo $a * $b;
+}
+
+function divide($a, $b) {
+    // Add code here
+    echo $a / $b;
+}
+
+function modulus($a, $b){
+	// Modulus
+	echo $a % $b;
+}
+
+
+divide(6, 6);
+// subtract($a,$b);
+// modulus($a,$b);
+// multiply($a,$b);
+// add($a,$b);
+
+
