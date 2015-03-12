341f3c74c77caf91d24e6839bf785a46833957a9
Benny Cardenas <benitocardenas@gmail.com>
2015-03-12 14:04:53 -0500

    Fuctions with Reurn vs echo

--- arithmetic.php
+++ arithmetic.php
@@ -6,25 +6,30 @@
 
 function add($a, $b) {
     if(is_numeric($a) && is_numeric($b)){
-    	echo $a + $b. PHP_EOL;
+    	// echo $a + $b. PHP_EOL;
+    	return $a + $b. PHP_EOL;
     } else {
-    	echo "Uh, oh.. try using a number";
+   	 	// echo "Uh, oh.. try using a number";
+   	 	errors($a, $b);
     }   
 }
 
 function subtract($a, $b) {
     // Add code here
 	if(is_numeric($a) && is_numeric($b)){
-    	echo $a - $b. PHP_EOL;
+    	// echo $a - $b. PHP_EOL;
+    	return $a - $b. PHP_EOL;
     } else {
-    	echo "Uh, oh.. try using a number";
+    	// echo "Uh, oh.. try using a number";
+    	errors ($a, $b);
     }  
 }
 
 function multiply($a, $b) {
     // Add code here
 	if(is_numeric($a) && is_numeric($b)){
-    	echo $a * $b. PHP_EOL;
+    	// echo $a * $b. PHP_EOL;
+    	return $a * $b. PHP_EOL;
     } else {
     	// echo "Uh, oh.. one of '$a' or '$b' isn't a number";
     	errors($a, $b);
@@ -34,8 +39,10 @@ function multiply($a, $b) {
 function divide($a, $b) {
     // Add code here
 	if ($b == 0){
-		echo "Uh, oh.. \$b is zero" . PHP_EOL;
+		// echo "Uh, oh.. \$b is zero" . PHP_EOL;
+		errorsDivide($a, $b);
 	} else {
+		// echo $a / $b. PHP_EOL;
 		echo $a / $b. PHP_EOL;
 	}
 }
@@ -43,9 +50,10 @@ function divide($a, $b) {
 function modulus($a, $b){
 	// Modulus
 	if(is_numeric($a) && is_numeric($b)){
-    	echo $a % $b . PHP_EOL;
+    	// echo $a % $b . PHP_EOL;
+    	return $a % $b . PHP_EOL;
     } else {
-    	echo "Uh, oh.. try using a number";
+    	errors($a, $b);
     }	
 }
 
@@ -53,9 +61,10 @@ function errors($a, $b){
 	echo "Uh, oh.. try using a number";
 	}
 
+function errorsDivide($a, $b){
+	echo "Oops, you can't divide by '0'";
+}
 
-
-// }
 
 // FOR SECOND SET OF EXERCISES
 // divide(5, 0);
@@ -63,13 +72,16 @@ function errors($a, $b){
 // subtract(30, 20);
 // add(25, 25);
 
-multiply(20, 'z');
+//VERIFIED FUNCTIONS WORKING WITH error function
+//AND ECHOING RETURN
+// echo multiply(20, 3);
+// echo divide(10, 5);
+// echo modulus(20, 7);
+// echo subtract (10,7);
+// echo add (10,7);
 
 
-
-
-
-// FOR FIRST SET OF EXERCISES
+// FOR FIRST SET OF EXERCISES - INITIAL
 // divide(6, 6);
 // subtract($a,$b);
 // multiply($a,$b);
