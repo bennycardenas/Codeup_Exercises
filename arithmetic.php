3390cc1e241ca9a94255782f967c1cb4cd34ea82
Benny Cardenas <benitocardenas@gmail.com>
2015-03-12 13:13:58 -0500

    Fuctions with Error function

--- arithmetic.php
+++ arithmetic.php
@@ -1,39 +1,81 @@
 <?php 
 
-$a = 4;
-$b = 7;
+// $a = 4;
+// $b = 7;
 
 
 function add($a, $b) {
-    echo $a + $b. PHP_EOL;
+    if(is_numeric($a) && is_numeric($b)){
+    	echo $a + $b. PHP_EOL;
+    } else {
+    	echo "Uh, oh.. try using a number";
+    }   
 }
 
 function subtract($a, $b) {
     // Add code here
-    echo $a - $b. PHP_EOL;
+	if(is_numeric($a) && is_numeric($b)){
+    	echo $a - $b. PHP_EOL;
+    } else {
+    	echo "Uh, oh.. try using a number";
+    }  
 }
 
 function multiply($a, $b) {
     // Add code here
-    echo $a * $b. PHP_EOL;
+	if(is_numeric($a) && is_numeric($b)){
+    	echo $a * $b. PHP_EOL;
+    } else {
+    	// echo "Uh, oh.. one of '$a' or '$b' isn't a number";
+    	errors($a, $b);
+    } 
 }
 
 function divide($a, $b) {
     // Add code here
-    echo $a / $b. PHP_EOL;
+	if ($b == 0){
+		echo "Uh, oh.. \$b is zero" . PHP_EOL;
+	} else {
+		echo $a / $b. PHP_EOL;
+	}
 }
 
 function modulus($a, $b){
 	// Modulus
-	echo $a % $b . PHP_EOL;
+	if(is_numeric($a) && is_numeric($b)){
+    	echo $a % $b . PHP_EOL;
+    } else {
+    	echo "Uh, oh.. try using a number";
+    }	
 }
 
+function errors($a, $b){
+	echo "Uh, oh.. try using a number";
+	}
 
+
+
+// }
+
+// FOR SECOND SET OF EXERCISES
+// divide(5, 0);
+// modulus(10, 5);
+// subtract(30, 20);
+// add(25, 25);
+
+multiply(20, 'z');
+
+
+
+
+
+// FOR FIRST SET OF EXERCISES
 // divide(6, 6);
 // subtract($a,$b);
 // multiply($a,$b);
-add(100, 2000);
+// add(100, 2000);
+// modulus(23, 79);
 
-modulus(23, 79);
+
 
 
