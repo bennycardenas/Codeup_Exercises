bcff3e63521c3f96b07712f5e997120dd84661a0
Benny Cardenas <benitocardenas@gmail.com>
2015-03-12 09:41:54 -0500

    Functions beginning Cleaned up

--- arithmetic.php
+++ arithmetic.php
@@ -5,34 +5,35 @@ $b = 7;
 
 
 function add($a, $b) {
-    echo $a + $b;
+    echo $a + $b. PHP_EOL;
 }
 
 function subtract($a, $b) {
     // Add code here
-    echo $a - $b;
+    echo $a - $b. PHP_EOL;
 }
 
 function multiply($a, $b) {
     // Add code here
-    echo $a * $b;
+    echo $a * $b. PHP_EOL;
 }
 
 function divide($a, $b) {
     // Add code here
-    echo $a / $b;
+    echo $a / $b. PHP_EOL;
 }
 
 function modulus($a, $b){
 	// Modulus
-	echo $a % $b;
+	echo $a % $b . PHP_EOL;
 }
 
 
-divide(6, 6);
+// divide(6, 6);
 // subtract($a,$b);
-// modulus($a,$b);
 // multiply($a,$b);
-// add($a,$b);
+add(100, 2000);
+
+modulus(23, 79);
 
 
